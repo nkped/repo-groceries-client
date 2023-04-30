ae960428fbf605c63b8ff916a4db63480b1d19c6
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-04-30 13:21:23 +0200

    all functions working with localstorage

--- src/App.js
+++ src/App.js
@@ -4,59 +4,60 @@ import { useState } from "react";
 
 function App() {
 
-  const [ items, setItems ] = useState([ 
-    {id: 1, checked: true, item: 'cookies'},
-    {id: 2, checked: false, item: 'coffee'},
-    {id: 3, checked: false, item: 'milk'}
-    ])
+  const [ items, setItems ] = useState(JSON.parse(localStorage.getItem('groceries')) || [] )
 
   const [ newItem, setNewItem ] = useState('')
 
 
-  const setAndSave = (item) => {
-    const listItems = [ ...items, item ]
-    setItems(listItems)
+  const setAndSave = (newItems) => {    
+    setItems(newItems)
+    localStorage.setItem('groceries', JSON.stringify(newItems) )
   } 
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1
     const myNewItem = { id, checked: false, item }
-    setAndSave(myNewItem)
-    console.log(myNewItem)
+    const listItems = [ ...items, myNewItem ]
+    setAndSave(listItems)
   }
-
 
   const handleSubmit = (e) => {    
     e.preventDefault()
-    console.log('submitted')
     addItem(newItem)
     setNewItem('')
   }
 
   const handleChecked = (id) => {
     const listItems = items.map((item) => ( item.id === id ? {...item, checked: !item.checked} : item ))
-    setItems(listItems)
+    setAndSave(listItems)
   }
 
   const handleDelete = (id) => {
       const listItems = items.filter((item) => item.id !== id )
-      setItems(listItems)
+      setAndSave(listItems)
   }
 
   return (
     <div className="App"><h1>Hello!</h1>
- <AddItem
-        newItem={newItem}
-        setNewItem={setNewItem}
-        handleSubmit={handleSubmit}
-      />
-  <Content 
-    items={items} 
-    handleChecked={handleChecked} 
-    handleDelete={handleDelete}
-    />
+      <AddItem
+              newItem={newItem}
+              setNewItem={setNewItem}
+              handleSubmit={handleSubmit}
+            />
+        <Content 
+          items={items} 
+          handleChecked={handleChecked} 
+          handleDelete={handleDelete}
+          />
     </div>
   );
 }
 
 export default App;
+
+
+/* [ 
+  {id: 1, checked: true, item: 'cookies'},
+  {id: 2, checked: false, item: 'coffee'},
+  {id: 3, checked: false, item: 'milk'}
+  ] */
