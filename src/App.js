123d118ec14fdd2711cb61ceacbb1b619ee7b630
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-05-01 11:41:59 +0200

    clean upbefore adding save to json

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import Content from "./Content";
 import AddItem from "./AddItem";
 import SearchItem from "./SearchItem";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 
-  const [ items, setItems ] = useState(JSON.parse(localStorage.getItem('groceries')) || [] )
+  const [ items, setItems ] = useState( [] )
+
+  //JSON.parse(localStorage.getItem('groceries')) ||
 
   const [ newItem, setNewItem ] = useState('')
 
   const [ search, setSearch ] = useState('')
 
+  const API_URL = 'http://localhost:3500/items'
+
 
   const setAndSave = (newItems) => {    
     setItems(newItems)
-    localStorage.setItem('groceries', JSON.stringify(newItems) )
+    //localStorage.setItem('groceries', JSON.stringify(newItems) )
   } 
+
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL)
+        if(!response.ok) throw Error('Did not receive todo-list data from json-server')
+        const listItems = await response.json()
+        setItems(listItems)        
+      } catch (err) {
+        console.error(err.message)        
+        }
+    } 
+    (async () => await fetchItems())()      
+  }, [])
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1
