b46ec3e631fee766218ad00427046d492c88c1d7
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-05-01 16:08:39 +0200

    crud ops finished

--- src/App.js
+++ src/App.js
@@ -76,9 +76,19 @@ function App() {
 
   }
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
       const listItems = items.filter((item) => item.id !== id )
-//      setAndSave(listItems)
+
+      setItems(listItems)
+
+      const deleteOptions = {
+        method: 'DELETE'
+      }
+
+      const reqUrl = `${API_URL}/${id}`
+
+      const result = await apiRequest(reqUrl, deleteOptions)
+      if (result) setFetchError(result)
   }
 
   return (
